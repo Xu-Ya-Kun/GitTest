9a06afcf00462696e7a054aaa9e489e8fe87c492
xuyakun <1741041442@qq.com>
2022-07-01 17:55:08 +0800

    push commit

--- src/com/xuyakun/implementation/Main.java
+++ src/com/xuyakun/implementation/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("hello world3!");
         System.out.println("hello world4!-hot-fix add master");
         System.out.println("hello world4!-hot-fix add hot-fix");
+        System.out.println("xuyakun");
     }
 }
