0e5c60e76ba733754bdf8b1e131b1b2735c76de7
xuyakun <1741041442@qq.com>
2022-07-01 10:07:18 +0000

    update src/com/xuyakun/implementation/Main.java.

--- src/com/xuyakun/implementation/Main.java
+++ src/com/xuyakun/implementation/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("hello world4!-hot-fix add hot-fix");
         System.out.println("xuyakun");
         System.out.println("xu");
+        System.out.println("yakun"); 
     }
 }
